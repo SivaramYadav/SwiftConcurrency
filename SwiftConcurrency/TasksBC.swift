7c134ded9301a5e166d51a123b64a29a239f7617
Sivaram Yadav <nsivaram248@gmail.com>
2024-08-27 19:37:09 +0530

    Added Task related code and usages

---
+++ SwiftConcurrency/TasksBC.swift
@@ -0,0 +1,18 @@
+//
+//  TasksBC.swift
+//  SwiftConcurrency
+//
+//  Created by Sivaram Yadav on 27/08/24.
+//
+
+import SwiftUI
+
+struct TasksBC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksBC()
+}
